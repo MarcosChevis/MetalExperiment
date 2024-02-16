7c4d9e3148ed281624e8117c51d57c44195d86f1
MarcosChevis <marcoschevism@gmail.com>
2024-02-16 17:20:16 -0300

    Initial Commit

---
+++ MetalExperiment/MetalExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalExperimentApp.swift
+//  MetalExperiment
+//
+//  Created by Marcos Chevis on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
