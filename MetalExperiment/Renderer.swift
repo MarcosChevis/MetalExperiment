f338e622da512f38914f44a0de1fae6ed087b0cc
MarcosChevis <marcoschevism@gmail.com>
2024-02-27 20:43:22 -0300

    updated

--- MetalExperiment/Renderer.swift
+++ MetalExperiment/Renderer.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Marcos Chevis on 22/02/24.
 //
-
 import MetalKit
 
 final class Renderer: NSObject, MTKViewDelegate {
@@ -13,41 +12,62 @@ final class Renderer: NSObject, MTKViewDelegate {
     var metalCommandQueue: MTLCommandQueue?
     var pipelineState: MTLRenderPipelineState?
     
-    var vertices: [Vertex] = []
     private var clearColor: MTLClearColor  = MTLClearColorMake(1.0, 1.0, 1.0, 1.0)
+    
+    var polygons: [RegularPolygon] =  {
+        var pols = [RegularPolygon]()
+        let numberOfPolygons = 1000
+        
+        let minValue: Float = -1.0
+        let maxValue: Float = 1.0
+
+        let vector = (0..<numberOfPolygons).map { index in
+            let t = Float(index) / Float(numberOfPolygons - 1)
+            return (1 - t) * minValue + t * maxValue
+        }
+        
+        for i in 0..<numberOfPolygons {
+            let center: simd_float2 = [vector[i], 0]
+            let radius: Float = 0.1
+            let amountOfSides: Int32 = 100
+            let color: simd_float4 = [0, 0, 0, 1]
+            
+            let polygon = RegularPolygon(center: center, radius: radius, amountOfSides: amountOfSides)
+            pols.append(polygon)
+        }
+        return pols
+    }()
     
     init(metalDevice: MTLDevice?) {
         self.metalDevice = metalDevice
         super.init()
         self.metalCommandQueue = metalDevice?.makeCommandQueue()
         self.pipelineState = makePipelineState()
-        vertices = []/*RegularPolygon(center: [-0.5, 0.0], radius: 0.5, amountOfSides: 1000).triangulated() + RegularPolygon(center: [0.5, 0.0], radius: 0.5, amountOfSides: 1000).triangulated()*/
-        
     }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
     
     func draw(in view: MTKView) {
-        guard let metalDevice,
+        guard let metalDevice = self.metalDevice,
               let drawable = view.currentDrawable,
               let renderPassDescriptor = view.currentRenderPassDescriptor,
               let commandBuffer = metalCommandQueue?.makeCommandBuffer(),
-              let pipelineState else {
+              let pipelineState = self.pipelineState else {
             preconditionFailure("Could not draw")
         }
         
         renderPassDescriptor.colorAttachments[0].clearColor = clearColor
-        // MARK: Isso aqui deve ajudar na otimização, não entendi 100%
         renderPassDescriptor.colorAttachments[0].loadAction = .clear
         renderPassDescriptor.colorAttachments[0].storeAction = .store
         
         guard let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else {
-            preconditionFailure("could not get renderer encoder")
+            preconditionFailure("Could not create render encoder")
         }
+        
         renderEncoder.setRenderPipelineState(pipelineState)
-        //                                                                                      Aqui tb otimizacao
-        let vertices = RegularPolygon(center: [-0.5, 0.0], radius: 0.5, amountOfSides: 100).triangulated() + RegularPolygon(center: [0.5, 0.0], radius: 0.5, amountOfSides: 100).triangulated()
-        let vertexBuffer = metalDevice.makeBuffer(bytes: vertices, length: vertices.getStride(), options: [])
+        
+        let vertices = polygons.flatMap { $0.triangulated() }
+        let vertexBuffer = metalDevice.makeBuffer(bytes: vertices, length: vertices.count * MemoryLayout<Vertex>.stride, options: [])
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
         renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
         
@@ -56,22 +76,27 @@ final class Renderer: NSObject, MTKViewDelegate {
         commandBuffer.present(drawable)
         commandBuffer.commit()
     }
-    
-    
 }
 
 // MARK: Pipeline State
 extension Renderer {
     
     private func makePipelineState() -> MTLRenderPipelineState? {
-        guard let library = metalDevice?.makeDefaultLibrary() else { preconditionFailure("could not get default library") }
-        let pipelineDescriptor = MTLRenderPipelineDescriptor()
-        pipelineDescriptor.vertexFunction = library.makeFunction(name: "vertexShader")
-        pipelineDescriptor.fragmentFunction = library.makeFunction(name: "fragmentShader")
-        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        guard let pipelineState = try? metalDevice?.makeRenderPipelineState(descriptor: pipelineDescriptor) else {
-            preconditionFailure("could not get pipeline state")
+        guard let library = metalDevice?.makeDefaultLibrary(),
+              let vertexFunction = library.makeFunction(name: "vertexShader"),
+              let fragmentFunction = library.makeFunction(name: "fragmentShader") else {
+            preconditionFailure("Could not get shader functions")
         }
-        return pipelineState
+        
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = vertexFunction
+        pipelineDescriptor.fragmentFunction = fragmentFunction
+        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        do {
+            return try metalDevice?.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        } catch {
+            preconditionFailure("Could not create pipeline state: \(error)")
+        }
     }
 }
